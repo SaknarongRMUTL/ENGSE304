6c136260c2c41f8a82083deb177b762f1063f507
SaknarongRMUTL <saknarong_2025@live.rmutl.ac.th>
2026-02-01 19:43:02 +0700

    Delete lab directory

--- lab/lab1.1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a, b, c;
-    int sum;
-    scanf("%d %d %d", &a, &b, &c);
-    sum = a + b + c;
-    printf("Answer = %d", sum);
-    return 0;
-}//end of main
--- lab/lab4.1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int Day;
-    printf("Input Days : ");
-    scanf("%d",&Day);
-    printf("%d days = %d seconds",Day,Day*86400);
-}//end main
